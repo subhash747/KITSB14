15cdd1bb929c8dbeef74dab722da048e542b0ce4
subhash747 <subhashgugloth681@gmail.com>
2024-07-18 00:03:24 +0530

    feature202 changes

--- Application.java
+++ Application.java
@@ -9,3 +9,13 @@
         "http://search.oracle.com/Admin",
         "AdminService"
 
+#feature202        // 6. Use object key above to get schedule state
+        oo = adminPort.getState(
+          "schedule",
+          objectKey,
+          null, // request all state properties
+          credentials,
+          null,
+          "en"
+ by mahi for Reports module in oct2023
+#end feature202
